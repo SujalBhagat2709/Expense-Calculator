d32cc13f33cfd67c5857b29fb2464fd18d0f4c08
Sujal Bhagat <167618538+SujalBhagat2709@users.noreply.github.com>
2026-04-29 09:33:05 +0530

    Add files via upload

---
+++ total_expense.py
@@ -0,0 +1,10 @@
+def calculate_total(expenses):
+    
+    return sum(expenses)
+
+
+if __name__ == "__main__":
+    
+    data = [250, 120, 80, 550]
+    
+    print("Total Expense:", calculate_total(data))
